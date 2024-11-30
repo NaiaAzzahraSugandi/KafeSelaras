79c7bc8361f4119d9f43a80e5a4408b27a3e1af9
Naia Azzahra Sugandi <naiaazzahra.2701@gmail.com>
2024-11-30 23:49:04 +0700

    Menambahkan folder gradle

---
+++ src/test/java/com/example/KafeSelaras/KafeSelarasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KafeSelaras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafeSelarasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
